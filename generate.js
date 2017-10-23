70e3e6b743023f2b866dc56f905ab5ba82ab300d
mirukuma <KOKI@KOKI-no-MacBook-Air.local>
2017-10-23 14:32:23 +0900

    入力を乱数に
    モデルを毎回ロードしてるのが気になるから次はそこを改善したい

--- generate.js
+++ generate.js
@@ -13,7 +13,7 @@ function generate(){ //Added by mirukuma ,Changed by JPNYKW ,Changed by mirukuma
 
 }
 
-function generate2() { //Added by mirukuma ,Changed by JPNYKW ,Changed by mirukuma ,Changed by JPNYKW
+function generate2() { //Added by mirukuma 
   const model = new KerasJS.Model({ //--Added by mirukuma
     filepaths: {
       model: './data/generator.json',
@@ -30,9 +30,14 @@ function generate2() { //Added by mirukuma ,Changed by JPNYKW ,Changed by miruku
       // or `input` for Sequential models
       // values are the flattened Float32Array data
       // (input tensor shapes are specified in the model config)
-      const inputData = {
-        'input': new Float32Array(100)
+      var noise= new Float32Array(100)
+      for(i=0; i<100; i=i+1){
+        noise[i]=Math.random()*2-1
       }
+      const inputData = {
+        'input': noise
+      }
+
       console.log(inputData)
       // make predictions
       return model.predict(inputData)
