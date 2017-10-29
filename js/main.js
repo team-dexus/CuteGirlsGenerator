8027b8f0b09b5396b447a029223e92c930d62941
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-10-29 16:46:36 +0900

    大幅アップデート

--- js/main.js
+++ js/main.js
@@ -10,18 +10,18 @@ window.onload=()=>{
 	canv.width=128;
 	canv.height=128;
 	
-	textRoll('-Generate ultimate cute girls-',d.getElementById('shel'),50
-		 ,'<h1>-Generate <span class="text">ultimate cute</span> girls.-</h1>');
+	textRoll('-Generate ultimate cute girls-',d.getElementById('display'),50
+		 ,d.getElementById('shel'),'<h1>-Generate <span class="text">ultimate cute</span> girls.-</h1>');
 }
 
-function textRoll(text,target,interval,afterInput){
+function textRoll(text,target,interval,subtarget,afterInput){
 	let cnt=0;
 	let roll=setInterval(()=>{
 		cnt++;
 		target.innerText=text.substr(0,cnt);
 		if(cnt>text.length){
 			clearInterval(roll);
-			if(afterInput!=void(0))target.innerHTML=afterInput;
+			if(afterInput!=void(0))subtarget.innerHTML=afterInput;
 		}
 	},interval);
 }
