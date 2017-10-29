980f5c82338a75653efac4b7e9ba6fdeffc16dba
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-10-29 20:54:23 +0900

    描画関数更新（ついに描画成功！！！！！！！！！！！！）

--- js/draw.js
+++ js/draw.js
@@ -4,10 +4,12 @@ CanvasRenderingContext2D.prototype.drawImageByDada=function($Array,$width,$heigh
 	for($hc=0;$hc<$height;$hc++){
 		let $dx=$x;
 		for($wc=0;$wc<$width;$wc++){
-			let $color=$Array[$idx];
-			$color=[$Array[$idx+0],$Array[$idx+1],$Array[$idx+2]];
-			$color.forEach((e,i)=>{$color[i]=e*127.5+127.5});
-			this.drawFillBox($dx,$dy,$px+1,`rgb(${~~$color[0]-70},${~~$color[1]},${~~$color[2]},)`);
+			let R=$Array[$idx+0]*127.5+127.5;
+			let G=$Array[$idx+1]*127.5+127.5;
+			let B=$Array[$idx+2]*127.5+127.5;
+			R=~~R+',';G=~~G+',';B=~~B+'';
+			if($idx==0)console.log([R,G,B]);
+			this.drawFillBox($dx,$dy,$px+1,`rgb(${R+G+B})`);
 			$dx+=$px;
 			$idx+=3;
 		}
