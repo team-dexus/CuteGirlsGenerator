fca609de608819a31492322a21edb0539477bb99
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-10-29 18:21:47 +0900

    keras用に改造

--- js/draw.js
+++ js/draw.js
@@ -1,12 +1,14 @@
-CanvasRenderingContext2D.prototype.drawImageByDada=function($Array,$width,$height,$px,$x,$y){
+CanvasRenderingContext2D.prototype.drawImageByDada=function($Array,$width,$height,$find,$px,$x,$y){
 	let $dy=$y;
+	let $find=0;
 	for($hc=0;$hc<$height;$hc++){
 		let $dx=$x;
 		for($wc=0;$wc<$width;$wc++){
-			let $color=$Array[$hc][$wc];
+			let $color=$Array[$find];
 			$color.forEach((e,i,a)=>{a[i]=e*127.5+127.5});
 			this.drawFillBox($dx,$dy,$px,`rgb(${$color[0]},${$color[1]},${$color[2]})`);
 			$dx+=$px;
+			$find++;
 		}
 		$dy+=$px;
 	}
