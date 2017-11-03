0a4869e0b5f36d169069a4f28c4bf48cca839a7e
JPNYKW <31243896+JPNYKW@users.noreply.github.com>
2017-11-03 21:46:57 +0900

    fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var d=document;
 var m=Math;
 
-var w,h,
+var w,h;
 var canv,cont;
 var generatedData; // generate2() の戻り値を格納する変数
 
@@ -17,22 +17,22 @@ window.onload=()=>{
 	
 	textRoll('-Generate ultimate cute girls-',d.getElementById('display'),50
 		 ,d.getElementById('shel'),'<h1>-Generate <span class="text">ultimate cute</span> girls.-</h1>');
+}
 
-	function textRoll(text,target,interval,subtarget,afterInput){
-		let cnt=0;
-		let roll=setInterval(()=>{
-			cnt++;
-			target.innerText=text.substr(0,cnt);
-			if(cnt>text.length){
-				clearInterval(roll);
-				if(afterInput!=void(0))subtarget.innerHTML=afterInput;
-			}
-		},interval);
-	}
+function textRoll(text,target,interval,subtarget,afterInput){
+	let cnt=0;
+	let roll=setInterval(()=>{
+		cnt++;
+		target.innerText=text.substr(0,cnt);
+		if(cnt>text.length){
+			clearInterval(roll);
+			if(afterInput!=void(0))subtarget.innerHTML=afterInput;
+		}
+	},interval);
+}
 
-	function setImgSize(){
-		wid=~~w.value;
-		hig=~~h.value;
-		console.log([w,h]); // test
-	}
+function setImgSize(){
+	wid=~~w.value;
+	hig=~~h.value;
+	console.log([w,h]); // test
 }
