671e08df7e4c7dd2afc89d4c4623a98a60c7742d
CommandSlash017 <31244102+CommandSlash017@users.noreply.github.com>
2018-02-06 00:20:38 +0900

    downloadHandle Update

--- js/main.js
+++ js/main.js
@@ -1,50 +1,46 @@
-var download,onGenerated=false;
-var fileIndex=-1;
-var d=document;
-var c=console;
-var w=window;
-var m=Math;
-
-var w,h;
-var canv,cont;
+var download, onGenerated = false;
+var fileIndex = 0;
+var d = document;
+var c = console;
+var w = window;
+var m = Math;
+var w, h;
+var canv, cont;
 var generatedData; // generate2() の戻り値を格納する変数
-
-window.onload=()=>{
-	download=d.getElementById("download");
-	canv=d.getElementById('output');
-	cont=canv.getContext('2d');
-
-	w=d.getElementById('w');
-	h=d.getElementById('h');
-
-	canv.width=168;
-	canv.height=280;
-
-	textRoll('-Generate ultimate cute girls-',d.getElementById('display'),50
-		 ,d.getElementById('shel'),'<h1 class="logo">-Generate <span class="text">ultimate cute</span> girls.-</h1>');
+window.onload = () => {
+	download = d.getElementById("download");
+	canv = d.getElementById('output');
+	cont = canv.getContext('2d');
+	w = d.getElementById('w');
+	h = d.getElementById('h');
+	canv.width = 168;
+	canv.height = 280;
+	textRoll('-Generate ultimate cute girls-', d.getElementById('display'), 50, d.getElementById('shel'), '<h1 class="logo">-Generate <span class="text">ultimate cute</span> girls.-</h1>');
 }
 
-function textRoll(text,target,interval,subtarget,afterInput){
-	let cnt=0;
-	let roll=setInterval(()=>{
+function textRoll(text, target, interval, subtarget, afterInput) {
+	let cnt = 0;
+	let roll = setInterval(() => {
 		cnt++;
-		target.innerText=text.substr(0,cnt);
-		if(cnt>text.length){
+		target.innerText = text.substr(0, cnt);
+		if (cnt > text.length) {
 			clearInterval(roll);
-			if(afterInput!=void(0))subtarget.innerHTML=afterInput;
+			if (afterInput != void(0)) subtarget.innerHTML = afterInput;
 		}
-	},interval);
+	}, interval);
 }
 
-function setImgSize(){
-	wid=~~w.value;
-	hig=~~h.value;
-	console.log([w,h]); // test
+function setImgSize() {
+	wid = ~~w.value;
+	hig = ~~h.value;
+	console.log([w, h]); // test
 }
 
-function downloadHandle(){
-	var DLImg=document.createElement("a")
-	DLImg.download="CGG.png"
-	DLImg.href=canv.toDataURL()
-	DLImg.click()
+function downloadHandle() {
+	if (onGenerated) {
+		var DLImg = document.createElement("a")
+		DLImg.download = `CGG${++fileIndex}.png`
+		DLImg.href = canv.toDataURL()
+		DLImg.click()
+	}
 }
